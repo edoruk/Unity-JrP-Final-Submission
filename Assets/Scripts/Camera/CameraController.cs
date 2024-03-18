72c353c6f5db6ae07145ea6f38584ff5a53511cb
Ege Doruk Sayin <egedoruksayin@gmail.com>
2024-03-18 22:57:24 +0300

    added movement animations
    -walk
    -run
    -jump

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
-    public float rotSpeed;
-    private Vector3 offsetPos = new Vector3(0, 1.5f, 2.5f);
-    
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +12,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SetCamera();
-    }
-
-    private void SetCamera()
-    {
-        // float horizontalInput = Input.GetAxis("Horizontal");
-        // transform.LookAt(player.transform.position);
-        // transform.RotateAround(player.transform.position, Vector3.up, horizontalInput * rotSpeed );
     }
 
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed = 2f;
-    private float rotationSpeed = 2f;
+    private float movementSpeed = 2.0f;
+    private float rotationSpeed = 2.0f;
+    public float _jumpForce = 5.0f;
     
     private Animator _animator;
     private UIController _uiControllerScript;
+    private Rigidbody _playerRb;
+
+    private bool isOnGround = true;
+    private bool isRunning = false;
     
 
     
@@ -19,6 +24,7 @@ public class PlayerController : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _uiControllerScript = GameObject.Find("UIManager").GetComponent<UIController>();
+        _playerRb = GetComponent<Rigidbody>();
 
     }
 
@@ -36,11 +42,28 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * verticalInput * movementSpeed * Time.deltaTime);
         transform.Rotate(Vector3.up * horizontalInput * rotationSpeed);
         
-        if(verticalInput == 0)
-            _animator.SetFloat("movementSpeed", 0);
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                _animator.SetFloat("movementSpeed",0.6f);
+            }
+            else
+            {
+                _animator.SetFloat("movementSpeed",0.3f);
+            }
+        }
         else
         {
-            _animator.SetFloat("movementSpeed",0.3f);
+            _animator.SetFloat("movementSpeed",0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            _playerRb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _animator.SetTrigger("jumpTrigger");
+            isOnGround = false;
         }
     }
 
@@ -59,5 +82,11 @@ public class PlayerController : MonoBehaviour
         {
             _uiControllerScript.ResetUsageText();
         }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+            isOnGround = true;
     }
 }
